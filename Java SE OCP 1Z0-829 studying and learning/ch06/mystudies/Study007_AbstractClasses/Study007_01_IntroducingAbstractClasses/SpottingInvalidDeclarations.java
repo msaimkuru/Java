5b35d15ee4861ca4b525792383b97eaec60be672
Saim Kuru <msaimkuru@gmail.com>
2022-08-01 16:24:35 +0100

    Update SpottingInvalidDeclarations.java

--- Java SE OCP 1Z0-829 studying and learning/ch06/mystudies/Study007_AbstractClasses/Study007_01_IntroducingAbstractClasses/SpottingInvalidDeclarations.java
+++ Java SE OCP 1Z0-829 studying and learning/ch06/mystudies/Study007_AbstractClasses/Study007_01_IntroducingAbstractClasses/SpottingInvalidDeclarations.java
@@ -9,93 +9,105 @@ package org.curlybrace.oopj.ocp1z0_829.ch06.mystudies.Study007_AbstractClasses.S
  */
 
 abstract class InvalidDeclarations{
-	/*
+	/* 	 
 	   public abstract long eat()
-	  
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule: Sbstract method declarations should end with a semicolon.
+	   ----------------
 	   error: ';' expected
        public abstract long eat()
                                  ^
-	   1 error
+       ----------------
 	 */
 	
-	/* Rule: abstract methods cannot have a body
-	
+	/* 	 	
 	   public abstract void swim() {};
-
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule:  Abstract methods cannot have a body
+	   ----------------
        error: abstract methods cannot have a body
 	   public abstract void swim() {};
 	                        ^
-       1 error
+       ----------------
 	 */
 	
 	/* 
 	   public abstract void sleep;
-	 
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule: Method declaration should have () for parameter list.
+	   ----------------
        error: '(' expected
 	   public abstract void sleep;
 	                             ^
-       1 error	 
+       ----------------	 
 	 */
 	
-	/* Rule: Concrete methods need to have an empty or full body. 
-	 
+	/* 	 
 	   public void goInShell();
-	   
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule: Concrete methods need to have an empty or full body. 
+	   ----------------	   
 	   error: missing method body, or declare abstract
 	   public void goInShell();
 	               ^
-       1 error
+       ----------------	 	               
      */
 }
 
 /* 
-   Rule: Java does not permit a class or method to be marked both abstract and final.
- 
    final abstract class InvalidDeclaration2{}
-
+   ----------------
+   DOES NOT COMPILE
+   Violated Rule: Java does not permit a class or method to be marked both abstract and final.
+   ----------------	   
    error: illegal combination of modifiers: abstract and final
    final abstract class InvalidDeclaration2{}
                   ^
-   1 error
+   ----------------	    
 */
 
 abstract class InvalidDeclaration3{
-	/*
-	   Rule: Java does not permit a class or method to be marked both abstract and final.
-	   	
+	/* 	
 	   public abstract final void walk();
-	   
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule: Java does not permit a class or method to be marked both abstract and final.
+	   ----------------	   	   
 	   error: illegal combination of modifiers: abstract and final
 	   public abstract final void walk();
 	                              ^
-       1 error
+	   ----------------		                              
      */  
 }
 
 abstract class InvalidDeclaration4{
 	/*
-	   Rule: A method cannot be marked as both abstract and private.
-	   
 	   private abstract void sing();
-	   
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule: A method cannot be marked as both abstract and private.
+	   ----------------		   
 	   error: illegal combination of modifiers: abstract and private
 	   private abstract void sing();
 	                         ^
-       1 error
 	 */
 }
 
 abstract class InvalidDeclaration5{
 	/*
-	   Rule: Only instance methods can be marked abstract within a class, not variables, 
-	   constructors, or static methods.
-	   
 	   public static abstract void swim();
-
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule: Only instance methods can be marked abstract within a class, not variables, 
+	   constructors, or static methods.
+	   ----------------	   
 	   error: illegal combination of modifiers: abstract and static
 	   public static abstract void swim();
 	                               ^
-       1 error
      */
 }
 
@@ -113,10 +125,12 @@ public class SpottingInvalidDeclarations {
 	
 	/*
 	   public abstract void abstractMethodInConcreteClassIsNotAllowed();
-
+	   ----------------
+	   DOES NOT COMPILE
+	   Violated Rule: Abstract methods cannot be declared in concrete classes. 
+	   ----------------	   
        error: SpottingInvalidDeclarations is not abstract and does not override abstract method abstractMethodInConcreteClassIsNotAllowed() in SpottingInvalidDeclarations
        public class SpottingInvalidDeclarations {
               ^
-       1 error
     */   
 }
